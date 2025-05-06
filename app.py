39fd95aff76fc4bfa0a86fbf29d00907c2c1338b
btbinh2710 <btbinh7@gmail.com>
2025-05-06 09:59:51 +0700

    Enable CORS to allow frontend access

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({"message": "Backend Flask hoạt động thành công!"})
+
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
