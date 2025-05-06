276888e6c215898cffff850c2096e47c11d835db
btbinh2710 <btbinh7@gmail.com>
2025-05-06 16:56:38 +0700

    Initial commit

---
+++ static/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const app = document.getElementById("app");
+    app.innerHTML = "<h1 class='text-center mt-5 text-2xl font-bold'>Hệ thống đã hoạt động thành công!</h1>";
+});
