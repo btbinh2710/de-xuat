9d54e066744fdb7f59bb29b2417ded428d979236
btbinh2710 <btbinh7@gmail.com>
2025-05-06 17:40:41 +0700

    Add gunicorn to requirements.txt and fix start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-set -e                            # có lỗi là dừng
-python create_db.py              # ➊ tạo (nếu chưa có) data.db + user
-exec gunicorn app:app            # ➋ chạy Gunicorn (sử dụng biến PORT của Render)
+set -e
+python create_db.py
+exec gunicorn app:app
